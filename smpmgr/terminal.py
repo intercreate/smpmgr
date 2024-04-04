c1e30ca6fcad01e11d4d1bade185e2428990eaf9
J.P. Hutchins <jphutchins@gmail.com>
2024-04-03 17:57:01 -0700

    refactor: reorganize terminal layout

--- smpmgr/terminal.py
+++ smpmgr/terminal.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import cast
+from typing import Final, cast
 
 import readchar
 import typer
@@ -7,42 +7,65 @@ from serial import Serial
 
 from smpmgr.common import Options
 
+MAP_KEY_TO_BYTES: Final = {
+    readchar.key.CTRL_C: b"\x03",
+    readchar.key.UP: b"\x1b[A",
+    readchar.key.DOWN: b"\x1b[B",
+    readchar.key.LEFT: b"\x1b[D",
+    readchar.key.RIGHT: b"\x1b[C",
+    readchar.key.ESC: b"\x1b",
+}
+
 
 def terminal(ctx: typer.Context) -> None:
     """Open a terminal to the device."""
 
     options = cast(Options, ctx.obj)
 
-    async def rx_from_device(port: Serial) -> None:
-        while True:
-            _bytes = port.read_all()
-            if _bytes is not None:
-                print(_bytes.decode(), end="", flush=True)
-            await asyncio.sleep(0.020)
-
-    def tx_keyboard_to_device(port: Serial) -> None:
-        while True:
-            char = readchar.readkey()
-            if char == readchar.key.CTRL_C:
-                raise KeyboardInterrupt
-            elif char == readchar.key.UP:
-                port.write(b"\x1b[A")
-            elif char == readchar.key.DOWN:
-                port.write(b"\x1b[B")
-            else:
-                port.write(char.encode())
-
     async def f() -> None:
+        if options.transport.port is None:
+            print("--port <port> option is required for the terminal, e.g.")
+            print("smpmgr --port COM1 terminal")
+            return
 
-        loop = asyncio.get_event_loop()
+        print(f"\x1b[2mOpening terminal to {options.transport.port}...", end="")
 
         with Serial(port=options.transport.port, baudrate=115200, timeout=options.timeout) as s:
-            device_result, keyboard_result = await asyncio.gather(
-                rx_from_device(s),
-                loop.run_in_executor(None, tx_keyboard_to_device, s),
-                return_exceptions=True,
+            print("OK")
+            print("Press Ctrl-T to exit the terminal.\x1b[22m")
+            print()
+            device_result, keyboard_result = await asyncio.wait(
+                (
+                    asyncio.create_task(_rx_from_device(s)),
+                    asyncio.create_task(asyncio.to_thread(_tx_keyboard_to_device, s)),
+                ),
+                return_when=asyncio.FIRST_EXCEPTION,
             )
 
         print(device_result, keyboard_result)
 
     asyncio.run(f())
+
+
+async def _rx_from_device(port: Serial) -> None:
+    """Async poll of the serial port for incoming data.
+
+    Can be replaced when pyserial is replaced."""
+
+    while True:
+        _bytes = port.read_all()
+        if _bytes is not None:
+            print(_bytes.decode(), end="", flush=True)
+        await asyncio.sleep(0.020)
+
+
+def _tx_keyboard_to_device(port: Serial) -> None:
+    """Blocking read of keyboard input."""
+    while True:
+        char = readchar.readchar()
+        if char == readchar.key.CTRL_T:
+            raise KeyboardInterrupt
+        try:
+            port.write(MAP_KEY_TO_BYTES[char])
+        except KeyError:
+            port.write(char.encode())
